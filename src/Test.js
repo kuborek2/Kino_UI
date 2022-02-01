6ef5a437c2cb61d45db3d06c6267209d62a2ba4a
kuborek2 <kuborek2@gmail.com>
2022-02-02 00:13:34 +0100

    Added server respone for films and seanses

--- src/Test.js
+++ src/Test.js
@@ -13,56 +13,94 @@ import soon1 from './soon1.json';
 import soon2 from './soon2.json';
 function Test() {
 
-  const [seans, setSeans] = useState([]);
-  const [film, setFilm] = useState([]);
-  const filmUrl = 'http://localhost:8080/api/v1/seanse';
-  const testUrl = 'http://localhost:8080/api/v1/seans?id=5';
+
+  const [dateButtonArray, setdateButtonArray] = useState([]);
+  const [currentMonth, setCurrentMonth] = useState(0);
+  const [currentDay, setCurrentDay] = useState(0);
+  const [seanses, setSeanses] = useState([]);
+  const [seansesToDisplay, setSeansesToDisplay] = useState([]);
+  const [date, setDate] = useState([]);
+  const [films, setFilms] = useState([]);
+  const filmsUrl = 'http://localhost:8080/api/v1/films';
+  const seasesUrl = 'http://localhost:8080/api/v1/seanse';
+  // const testUrl = 'http://localhost:8080/api/v1/seans?id=5';
 
   React.useEffect(() => {
-      axios.get(filmUrl).then((response) => {
-          setFilm(response.data);
-      });
-    }, []);
-  
- 
+    if ( currentMonth== 0 && currentDay == 0 ){
+      let date = new Date();
+      setCurrentMonth(date.getUTCMonth() + 1); //months from 1-12 as current moonth
+      setCurrentDay(date.getUTCDate()); // current day
+    }
+    dateButtonArraySetter();
+      axios.get(filmsUrl).then((response, config) => {
+          setFilms(response.data); });
+      axios.get(seasesUrl).then((response) => {
+        setSeanses(response.data);
+      }).then(() => {checkCurrentSeanses();});
+    }, [seanses], [films]);
 
-  
+  const checkElemntInTable = (array ,value) => {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array[i].length; j++) {
+        if( array[i][j] != null)
+          if( array[i][j].idf == value.idf )
+            return [i,j];
+      }
+    }
+    return -1;   // Not found
+  }
 
+  const dateButtonArraySetter = () => {
+    let tempArray = new Array(5);
+    var date = new Date();
+    for( let i = 0; i < dateButtonArray.length; i++ ){
+      let string = "";
+      if( i == 0 )
+        date.setDate(date.getDate() + 0);
+      else
+        date.setDate(date.getDate() + 1);
+      let month = date.getUTCMonth() + 1; //months from 1-12 as current moonth
+      let day = date.getUTCDate(); // current day
+      tempArray[i] = string.concat(month,'.',day);
+    }
+    setdateButtonArray(tempArray);
+  }
 
-  const [data,setData] = useState([
-    {
-      image:
-        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/kZNHR1upJKF3eTzdgl5V8s8a4C3.jpg',
-      title: 'Scream',
-      link : "https://www.themoviedb.org/movie/646385-scream",
-      seans1: '11:30 2D NAPISY',
-      seans2: '16:50 2D NAPISY'
-    },
-    {
-      image:
-        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg',
-      title: 'Spider-Man: No Way Home',
-      link : "https://www.themoviedb.org/movie/634649-spider-man-no-way-home",
-      seans1: '13:30 2D DUBBING',
-      seans2: '20:00 2D NAPISY'
-    },
-    {
-      image:
-        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/nj5HmHRZsrYQEYYXyAusFv35erP.jpg',
-      title: "The King's Man",
-      link : "https://www.themoviedb.org/movie/476669-the-king-s-man",
-      seans1: '15:00 2D NAPISY',
-      seans2: '17:30 2D NAPISY'
-    },
-    {
-      image:
-        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8c4a8kE7PizaGQQnditMmI1xbRp.jpg',
-      title: 'The Matrix Resurrections',
-      link : "https://www.themoviedb.org/movie/624860-the-matrix-resurrections",
-      seans1: '12:15 2D NAPISY',
-      seans2: '18:30 2D NAPISY'
-    },
-  ])
+  const changeCurrentData = (valueInDays) => {  
+    var date = new Date();
+    date.setDate(date.getDate() + valueInDays);
+    setCurrentMonth(date.getUTCMonth() + 1); //months from 1-12 as current moonth
+    setCurrentDay(date.getUTCDate()); // current day
+  }
+
+  const checkCurrentSeanses = () => {
+    console.log("currentDay: ",currentDay)
+    console.log("currentmonth: ",currentMonth)
+    let currSeanesToDisplay = [];
+
+    const m = 4;
+    const n = 2; 
+    let currentSeansesTable = new Array(m); // create an empty array of length n
+    // makeing arrray 2 dimmesinal
+    for (var i = 0; i < m; i++) {
+      currentSeansesTable[i] = new Array(n); // make each element an array
+    }
+    if( seanses != undefined ){
+      seanses.map((seansElement) => {
+        let seansDate = seansElement.dzien.split("-")
+        if( currentMonth == parseInt(seansDate[1]) ){
+          if( currentDay == parseInt(seansDate[2]) ){
+
+            currSeanesToDisplay.push(seansElement)
+          // end of day if
+          }
+        // end of month if
+        }
+      // end of map
+      })
+    setSeansesToDisplay(currSeanesToDisplay);
+    }
+  }
 
   return (
     <div className='cont'>
@@ -71,37 +109,38 @@ function Test() {
       </div>
       <div class="cos">
       <div class="cal">
-      <button class="card" onClick={()=>{setData(rep1)}}>
+      <button class="card" onClick={()=>{changeCurrentData(0)}}>
         <h7 class="date">Czw</h7>
-        <h7>27.01</h7>
+        <h7>{dateButtonArray[0]}</h7>
       </button>
-      <button class="card" onClick={()=>{setData(rep2)}}>
+      <button class="card" onClick={()=>{changeCurrentData(1)}}>
         <h7 class="date">Pt</h7>
-        <h7>28.01</h7>
+        <h7>{dateButtonArray[1]}</h7>
       </button>
-      <button class="card" onClick={()=>{setData(rep3)}}>
+      <button class="card" onClick={()=>{changeCurrentData(2)}}>
         <h7 class="date">Sb</h7>
-        <h7>29.01</h7>
+        <h7>{dateButtonArray[2]}</h7>
       </button>
-      <button class="card" onClick={()=>{setData(rep2)}}>
+      <button class="card" onClick={()=>{changeCurrentData(3)}}>
         <h7 class="date">Nd</h7>
-        <h7>30.01</h7>
+        <h7>{dateButtonArray[3]}</h7>
       </button>
-      <button class="card" onClick={()=>{setData(rep1)}}>
+      <button class="card" onClick={()=>{changeCurrentData(4)}}>
         <h7 class="date">Pn</h7>
-        <h7>31.01</h7>
+        <h7>{dateButtonArray[4]}</h7>
       </button>
     </div>
       <div class="row">
         
-        {data.map(cos =>(
-          <div   class="column">
-          <h2>{cos.title}</h2>
+        {seansesToDisplay.map(cos =>(
+          <div class="column">
+          <h2>{cos.tytul}</h2>
           <a href={cos.link} target="_blank" rel="noopener noreferrer">
-          <img style={{paddingLeft: "15%"}} src={cos.image}></img>
+          <img style={{paddingLeft: "15%"}} src={cos.zdjecie}></img>
           </a>
-          <h3><a  style={{textDecoration: "none"}} href="tickets">{cos.seans1}</a></h3>
-          <h3><a  style={{textDecoration: "none"}} href="tickets">{cos.seans2}</a></h3>
+          {}
+          {/* <h3><a  style={{textDecoration: "none"}} href="/">{cos.seans1}</a></h3>
+          <h3><a  style={{textDecoration: "none"}} href="/">{cos.seans2}</a></h3> */}
         </div>
         ))}
         
@@ -111,33 +150,20 @@ function Test() {
         <h1 style={{marginLeft:"4%"}}>Wkrótce</h1>
       </div>
       <div class="cos">
-      <div class="row">
-       
-        
-        
-      </div>
-      <div style={{marginTop: "2%"}} class="rowBot">
-      <div>
-        <div className="mapFilms">
-          {film.map(film1 =>(
-            <div   class="column">
-            <span className='filmTitle'>{film1.tytul}</span>          
-            <img style={{paddingTop:"10%",paddingLeft: "15%"}} src={film1.zdjecie}></img>   
-          
-          </div>
-          ))}
+        <div class="row">
+          {films.map(cos =>(
+            <div class="column">
+              <h2>{cos.tutyl}</h2>
+              <a href={cos.zdjecie} target="_blank" rel="noopener noreferrer">
+                <img style={{paddingLeft: "10%"}} src={cos.zdjecie}></img>
+              </a>
+            </div>
+          ))} 
+
         </div>
+
       </div>
-      {/* {soon2.map(cos =>(
-          <div class="column">
-            <h2>{cos.title}</h2>
-            <a href={cos.link} target="_blank" rel="noopener noreferrer">
-              <img style={{paddingLeft: "10%"}} src={cos.image}></img>
-            </a>
-          </div>
-        ))}  */}
-      </div>
-      </div>
+
     </div>
   );
 }
